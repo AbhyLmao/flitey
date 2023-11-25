2f566be16a51968c18b127cec3f7f20006cc592f
Abhy <artsyabhy@gmail.com>
2023-11-25 16:19:14 -0700

    upload

---
+++ src/main/java/com/termproj/Flitey/FliteyApplication.java
@@ -0,0 +1,13 @@
+package com.termproj.Flitey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliteyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliteyApplication.class, args);
+	}
+
+}
